a8bce0ade0d4234d36409398c0b2cafc6c4eae37
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-10-19 16:59:29 +0000

    templates added

--- run.py
+++ run.py
@@ -10,6 +10,16 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
